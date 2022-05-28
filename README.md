4674e18886ca71da6e9d6d869e96e1fc48b3ac6d
Jeanette Ralph <jean.ralph@hotmail.com>
2022-05-28 15:16:18 -0700

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Code-Quiz
+# Coding-Quiz
+## Created a coding quiz where after each question is clicked a new question appears and then the user can submit their initials and their score is saved in local storage. 
--- script.js
+++ script.js
@@ -1,130 +1,199 @@
 // defining variables globally
 var secondsLeft = 90;
+var timeEl = document.querySelector("#timer");
 var startBtn = document.getElementById('start');
-var optionBtn = document.querySelector('input[type=button]');
-var timeEl = document.querySelector(".timer");
+var submitBtn = document.getElementById('submit');
+var questionsEl = document.createElement('h1');
+var answersEl = document.createElement('h2');
+var answerBtn = document.createElement('button');
 var quizContainer = document.createElement('div');
-var questionsEl = document.createElement('p');
+var questionIndex = 0;
+var finalScore = 0; 
+var timerInterval; 
+var timeEl = document.querySelector("#timer");
+var highScoreInitials;
+var userHighScore;
+var highScoreDisplay = document.getElementById("high-score-page");
 
-// created an arrraay for questions to loop through
+
+
+
+// created an array for questions to loop through
 var quizQuestions = [ 
 
     { 
         question: 'A ____ is a special variable, that can hold more than one value.',
         answers: ['string', 'array', 'function'],
-        rightAnswer: 1, 
+        rightAnswer: 'array', 
     },
 
     { 
         question: 'What does flexbox help with ?',
         answers: ['makes it easier to design flexible responsive layout structure without using float or positioning', 'creating containers', 'styling the background'],
-        rightAnswer: 0, 
+        rightAnswer: 'makes it easier to design flexible responsive layout structure without using float or positioning', 
     },
 
     {
         question: 'HTML is a ____ language.',
         answers: ['programming', 'styling', 'markdowm'],
-        rightAnswer: 2, 
+        rightAnswer: 'markdowm', 
     },
 
     {
         question: 'What does a function do ?',
         answers: ['can have properties','is a block of code designed to perform a particular task','can loop through elements'],
-        rightAnswer: 1, 
+        rightAnswer: 'is a block of code designed to perform a particular task', 
     },
 
     {
         question: 'What is a boolean ?',
         answers: ['loops through an array','represents one of two values: true or false','a variable that must have a value assigned when declared'],
-        rightAnswer: 1, 
+        rightAnswer: 'represents one of two values: true or false', 
     },
 
-]
+];
 
+// displaying the questions and buttons with the answers
 function displayQuestions() {
-    startBtn.addEventListener("click",displayQuestions);
-    for (var i=0; i < quizQuestions.length; i++){
-        
-        setTime();
-        console.log('hello world1');
 
-        // create container div this will have question/answer
-        var quizContainer = document.createElement('div');
-        quizContainer.innerHTML = quizQuestions.textContent;
+    // create a h1 tag for questions + text content
+    var questionsEl = document.createElement('h1');
+    questionsEl.textContent = quizQuestions[questionIndex].question;
 
-        // create a p tag for questions/answers
-        var questionsEl = document.createElement('p');
-        questionsEl;
+    // appending questionEl to generate quiz div
+    document.getElementById('generate-quiz').append(questionsEl);
 
-        // appending questionEl to quizContainer
-        quizContainer.appendChild(questionsEl);
+    // appending answerEL to generate quiz div
+    document.getElementById('generate-quiz').append(answersEl);
 
-         // appending the quiz questions array to the questionsEl
-         questionsEl.innerHTML = quizQuestions.textContent;
-         document.body.appendChild(questionsEl);
-         console.log('hello world2');
+    // loop to create answer button an
+    for (var i=0; i < quizQuestions[questionIndex].answers.length; i++){
 
-        // get quiz questions at index i
-        quizQuestions[i];
-        console.log('hello world3');
+        // where we are in the loop
+        var answerOption = quizQuestions[questionIndex].answers[i];
 
-        // appending quizContainer to generate quiz div in html
-        var generateQuizDiv = document.getElementById("generate-quiz").innerHTML += quizContainer; 
-        generateQuizDiv;
+        console.log(quizQuestions[questionIndex]);
 
-        console.log('hello world4');
-       
+        // creating buttons for the quiz
+        var answerBtn = document.createElement('button');
 
-        for (var i=0; i < quizQuestions.length; i++) {
-            console.log('hello world5');
-            quizContainer;
+        // set value attribute to answer button
+        answerBtn.setAttribute('value', answerOption);
 
-        // for loop over answers for each question at i
-        // radio button/label to select just one
+        answerBtn.textContent = answerOption;
 
-            // add data attribute with value of i 
-            // append radio buttons to quiz container
+        // new function for click event 
+        answerBtn.addEventListener('click', checkAnswer);
 
-        }
+        // appending answer button to generate quiz div in html
+        document.getElementById('generate-quiz').append(answerBtn);
+    }
+}
 
+// checking if the answer is right or wrong
+function checkAnswer(event) {
+    console.log(event.target);
+    var clickedAnswer = event.target.value;
+
+    if (clickedAnswer !== quizQuestions[questionIndex].rightAnswer) {
+        secondsLeft -= 10;
+    }
+
+    else {
+        finalScore += 10;
+        document.getElementById('final-score').textContent = finalScore
+    }
+
+    questionIndex++;
+
+    // check for seeing if we are on the last question
+    if (questionIndex == quizQuestions.length) {
+        endQuiz();
+    }
+
+    // starting the game over
+    else {
+        displayQuestions();
     }
 
 }
 
-displayQuestions();
-
-
-// create a function for the timer
+// function for the timer 
 function setTime() {
-    startBtn.addEventListener("click",displayQuestions);
-    var timeEl = document.querySelector(".timer");
-    var secondsLeft = 90;
     
     // Sets interval in variable
-    var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         secondsLeft--;
-        timeEl.textContent = "Time Remaining: " + secondsLeft;
+        timeEl.textContent = secondsLeft;
 
-        if(secondsLeft === 0) {
-        // Stops execution of action at set interval
+        if(secondsLeft <= 0) {
+        // Stops the timer if it reaches this conditional
         clearInterval(timerInterval);
-        // Calls function to create and append image
+        // added an alert prompt
         alert('Time is up !');
         }
   }, 1000);
 }
 
 
+// select submit button and assign save score to it
+function saveScore() {
+    // get the initials from the input
+    var initials = document.getElementById("user-initials").value.trim();
+        // .trim() removes extra whitespace, prevents user from entering just spaces
+    
+    if (initials) {
+        // get high scores from local storage or set to empty array if none exist
+        var allScores = JSON.parse(window.localStorage.getItem('highScores')) || [];
+        
+        var newScore = {
+            // user initials
+            initials, 
+            // score
+            score: finalScore 
+        }
+        
+        // adding it to the existing scores
+        allScores.push(newScore);
+        // saves to local storage
+        window.localStorage.setItem('highScores', JSON.stringify(allScores));
 
-// <!-- GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score -->
+
+    }
+
+    // variables for the initials and high score
+    highScoreInitials = document.getElementById('high-score-initials').textContent;
+    userHighScore = document.getElementById('high-score').textContent;
+
+
+    // calling the high score to be displayed
+    var highScoreDisplay = document.getElementById("high-score-page");
+    highScoreDisplay = highScoreInitials + userHighScore;
+    highScoreDisplay.style.display = 'block';
+
+}
+
+// creating a new function to combine the other functions to make it easier
+function startQuiz() {
+    setTime();
+    displayQuestions();
+}
+
+function endQuiz() {
+    clearInterval(timerInterval);
+
+    var scoreDisplay = document.getElementById('end-score');
+
+    // will show the end-score div as it is previously hidden with the css
+    scoreDisplay.style.display = 'block';
+   
+}
+
+
+// event listener for start button
+startBtn.addEventListener("click",startQuiz);
+
+
+// event listener for submit button
+submitBtn.addEventListener('click',saveScore);
 
